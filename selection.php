af1b32ffee068f8063e8bdc387f6ce50a5233607
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-03 22:19:05 +0200

    Added selection sort part

--- selection.php
+++ selection.php
@@ -1,9 +1,50 @@
 <?php
 
+function selectionsort(&$Array, $n) {
+    for($i=0; $i<$n; $i++) {
+      $min_idx = $i;
+  
+      for($j=$i+1; $j<$n; $j++) {
+        if($Array[$j] < $Array[$min_idx])
+        {$min_idx = $j;}
+      }
+  
+      $temp = $Array[$min_idx];
+      $Array[$min_idx] = $Array[$i];
+      $Array[$i] = $temp;
+    }
+  }
+  
+  // function to print array
+  function PrintArray($Array, $n) { 
+    for ($i = 0; $i < $n; $i++) 
+      echo $Array[$i]." ";
+    echo "\n"; 
+  }  
+  
+  // test the code
+//   $MyArray = array(1, 10, 23, 50, 4, 9, -4);
+//   $n = sizeof($MyArray); 
+//   echo "Original Array\n";
+//   PrintArray($MyArray, $n);
+  
+//   selectionsort($MyArray, $n);
+//   echo "\nSorted Array\n";
+//   PrintArray($MyArray, $n);
 
+$word=$_GET["word"];
 
+$array = [];
 
+for ($i=0; $i < strlen( $word); $i++) { 
+   if ($word[$i]!==",") {
+    array_push($array,$word[$i]);
+   }
+}
+$s= sizeof($array);
+selectionsort($array,$s);
 
-
+$selected=join(",",$array);
+echo $selected;
 
 ?>
