53cc78a8f83d144ed30e40e722591af2c85412f9
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-03 23:19:15 +0200

    Palindrome attempt from js

--- palindrome.php
+++ palindrome.php
@@ -1,7 +1,23 @@
 <?php
 
-$word=$_GET{"word"}
+$word=$_GET["word"];
 
+function check($words) {
+    for ($i = 0; $i < strlen($words) ; $i++) {
+      $j =  strlen($words) - 1 - $i;
+      if ($words[$i] == $words[$j]) {
+        $portion = array_slice($i + 1, $j - 1);
+        check($portion);
+        if (strlen($portion) = 1) {
+          echo True;
+        }
+      } else {
+        echo false;
+        break;
+      }
+    }
+  }
+  
 
 
 
