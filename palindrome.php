0ffb0e30b03aa0bd68e485225acb35376ff815d6
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-04 22:30:35 +0200

    made palindrome using nested loops

--- palindrome.php
+++ palindrome.php
@@ -1,25 +1,38 @@
 <?php
 
 $word=$_GET["word"];
-
+$checker=0;
 function check($words) {
     for ($i = 0; $i < strlen($words) ; $i++) {
-      $j =  strlen($words) - 1 - $i;
-      if ($words[$i] == $words[$j]) {
-        $portion = array_slice($i + 1, $j - 1);
-        check($portion);
-        if (strlen($portion) = 1) {
-          echo True;
+      for ($j=strlen($words); $j >=0; $j--) { 
+        if ($words[$i] == $words[$j]) {
+          $checker++;
         }
-      } else {
-        echo false;
-        break;
+        else{
+          $checker=0;
+        }
       }
     }
-  }
+    if ($checker== strlen($words)/2){
+      echo "Palindrome";
+    }
+    else echo"NOT Palindrome";
   
-
-
-
-
+}
+  check($word)
 ?>
+
+
+//   $j =  strlen($words) - 1 - $i;
+    //   if ($words[$i] == $words[$j]) {
+    //     $portion = array_slice($i + 1, $j - 1);
+    //     check($portion);
+    //     if (strlen($portion) == 1) {
+    //       echo True;
+    //     }
+    //    else {
+    //     echo false;
+    //     break;
+    //    }
+    //   }
+    // 
