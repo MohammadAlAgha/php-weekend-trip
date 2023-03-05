30439d4b9c865e3c443d36898fb0cc215fefed7a
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-05 23:39:48 +0200

    Added the consonants.php

--- consonants.php
+++ consonants.php
@@ -1,9 +1,27 @@
 <?php
 
+$word=$_POST["word"];
 
-
-
-
-
-
+function consonants($words) {
+    $str = "";
+    $newest ="";
+    for ($i = 0; $i < strlen($words); $i++) {
+      if (
+        $words[$i] != "a" &&
+        $words[$i] != "o" &&
+        $words[$i] != "e" &&
+        $words[$i] != "i" &&
+        $words[$i] != "u"
+      ) {
+        $str.=$words[$i];
+      } else {
+        break;
+      }
+      $newest = substr($words,$i + 1);
+    }
+     $response=$newest.$str."ay";
+     echo $response;
+  }
+  consonants($word);
+  
 ?>
--- prime.php
+++ prime.php
@@ -1,9 +1,23 @@
 <?php
+$year=$_POST["year"];
 
+function primeEven($birth){
+    $ourYear= date("Y");
+    $response = [
+        "even" => "false",
+        "prime" => "false"
+    ];
+    $age=$ourYear-$birth;
+     if($age%2==0){
+            $response["even"] = "true";
+        }
+     for($i=2; $i<=$age; $i++){
+        if(($age % $i) !== 0){
+            $response ["prime"] = "true";
+        }}
 
-
-
-
-
+  echo json_encode($response);
+}
+primeEven($year);
 
 ?>
