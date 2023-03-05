8dcdf0ee97ae68642825ccb0fbd083d5ef0c42d2
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-05 21:16:59 +0200

    Added reverse word

--- reverse.php
+++ reverse.php
@@ -1,9 +1,27 @@
 <?php
 
+$word=$_GET['word'];
+function reverse($mix){
+    $separate=str_split($mix);
+    $pure=[];
+    for ($i=0; $i <count($separate); $i++) { 
+        if(!is_numeric($separate[$i])){
+            array_push($pure,$separate[$i] );
+            $separate[$i]="#";
+        }
+    }
+    $reversedPure=array_reverse($pure);
+    for ($i=0; $i < count($separate) ; $i++) { 
+        for ($j=0; $j < count($reversedPure) ; $j++) { 
+            if ($separate[$i]=="#") {
+               $separate[$i]=$reversedPure[$j];
+               array_shift($reversedPure);
+            }
+        }
+    }
+    $str=implode($separate);
+    echo $str;
+}
 
-
-
-
-
-
+echo reverse($word);
 ?>
